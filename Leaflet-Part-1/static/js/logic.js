f5f93818c42104f89fec47457304d8a6ec399050
m-janssens-boop <127706155+m-janssens-boop@users.noreply.github.com>
2023-07-04 22:08:03 -0700

    createFeatures function written

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,20 +1,39 @@
 let myMap = L.map("map", {
-    center: [39.82, -98.58],
+    center: [39.05, -110.24],
     zoom: 5
   });
   
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
   
-  let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
   
-  d3.json(url).then(function(response) {
+
+//create the createFeatures function to log specific features from each earthquake in the dataset
+function createFeatures(earthquakeData) {
+    //give each feature a popup that describes the place, time, and magnitude of the earthquake
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><h3>${feature.properties.mag}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+
+    //create a geoJSON layer that contains the features array on the earthquakeData object
+    //run the onEachFeature function once for each object in teh array
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+
+}
+d3.json(url).then(function(response) {
   
     console.log(response);
     features = response.features;
-  
+    
+    //send response to the createFeatures function
+    createFeatures(features);
+
+    //build an array for the location of each earthquake
     let quakeArray = [];
   
     for (let i = 0; i < features.length; i++) {
@@ -26,10 +45,7 @@ let myMap = L.map("map", {
   
     }
   
-    let quake = L.heatLayer(quakeArray, {
-      radius: 20,
-      blur: 35
-    }).addTo(myMap);
+
   
   });
   
